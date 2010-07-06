9e76cbd8ba4d6d1d9f0397fda9e18018ccccb312
Ben Congleton <ben@hab.la>
2010-07-06 00:41:40 -0400

    added everything

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  layout("default")
+  def rsvp
+  end
+  
+  def index
+    
+  end
+  
+  def registry
+    
+  end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,9 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'compass' , :version => '= 0.8.17'
+  config.gem 'haml', :version => '>=2.2.4'
+
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
@@ -19,7 +22,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-
+  #config.gem "compass"
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
