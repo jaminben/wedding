cfb9a0139d81473619a1cdb15a0f2cb6ddebe7e7
Ben Congleton <ben@hab.la>
2010-07-08 04:11:25 -0400

    fix haml plugin

--- vendor/plugins/haml/init.rb
+++ vendor/plugins/haml/init.rb
@@ -1,10 +1,6 @@
 require 'rubygems'
+gem 'haml', ' = 2.2.4'
 
-begin
-  require File.join(File.dirname(__FILE__), 'lib', 'haml') # From here
-rescue LoadError
-  gem 'haml', ' = 2.2.4'
-end
 
 # Load Haml and Sass
 Haml.init_rails(binding)
