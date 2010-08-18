cb67d0ef9a95d51b61d1348527003419d7ec6289
Ben Congleton <ben@hab.la>
2010-08-18 14:22:31 -0400

    added honeymoon registries

--- vendor/plugins/haml/init.rb
+++ vendor/plugins/haml/init.rb
@@ -1,6 +1,8 @@
-require 'rubygems'
-gem 'haml'
-
+begin
+  require File.join(File.dirname(__FILE__), 'lib', 'haml') # From here
+rescue LoadError
+  require 'haml' # From gem
+end
 
 # Load Haml and Sass
 Haml.init_rails(binding)
